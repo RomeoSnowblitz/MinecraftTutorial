dab38a2abb33e13454de7c692b8192b48fc28a1f
RomeoSnowblitz <6474snow@gmail.com>
2023-02-02 00:40:53 -0500

    Initial commit

---
+++ src/main/java/net/romeosnowblitz/hmh3/Hmh3.java
@@ -0,0 +1,14 @@
+package net.romeosnowblitz.hmh3;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hmh3 implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("hmh3");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
