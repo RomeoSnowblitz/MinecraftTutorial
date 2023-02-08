f9972c939945aa6fcb0c008857a4bfce44d2a7ab
RomeoSnowblitz <6474snow@gmail.com>
2023-02-08 14:55:45 -0500

    Block Settings

--- src/main/java/net/romeosnowblitz/hmh3/block/ModBlocks.java
+++ src/main/java/net/romeosnowblitz/hmh3/block/ModBlocks.java
@@ -2,21 +2,39 @@ package net.romeosnowblitz.hmh3.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.romeosnowblitz.hmh3.Hmh3;
 import net.romeosnowblitz.hmh3.item.ModItemGroup;
 
 public class ModBlocks {
 
-    public static final Block CEMENT = registerBlock("cement", new Block(AbstractBlock.Settings.of(Material.STONE)), ModItemGroup.MOD_BLOCKS);
+    public static final Block CEMENT = registerBlock("cement", new Block(AbstractBlock.Settings.of(Material.STONE).requiresTool()), ModItemGroup.MOD_BLOCKS);
+
+
+    private static Boolean never(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
+        return false;
+    }
+
+    private static Boolean always(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
+        return true;
+    }
+
+    private static boolean always(BlockState state, BlockView world, BlockPos pos) {
+        return true;
+    }
+
+    private static boolean never(BlockState state, BlockView world, BlockPos pos) {
+        return false;
+    }
 
     public static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
